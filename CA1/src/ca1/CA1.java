4c48cc884fe8df082f72a7bd0be650c5d24f5d40
Farley Reis <61804811+farleyreis@users.noreply.github.com>
2020-11-29 21:54:00 +0000

    test conectividade

---
+++ CA1/src/ca1/CA1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca1;
+
+/**
+ *
+ * @author farleyreis
+ */
+public class CA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("test");
+        // TODO code application logic here
+    }
+    
+}
